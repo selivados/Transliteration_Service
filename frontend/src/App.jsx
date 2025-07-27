8bd80a76910ec9984eaf2fd2b57fba090ff731ee
Anton <selivados@gmail.com>
2025-07-27 22:00:03 +0800

    Improved code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,78 @@
 import { useState } from "react"
-
-import table from "../../translit_table.json"
+import axios from "axios"
 
 export default function App() {
   const [text, setText] = useState("");
+  const [translit, setTranslit] = useState("");
 
-  const transliterate = (text) => {
-    return text.split("").map(char => table[char] || char).join("");
-  };
+  const handleClear = () => {
+    setText("")
+    setTranslit("")
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post("http://localhost:8000/api", { data: text });
+      setTranslit(response.data.data);
+    } catch (error) {
+      console.error("Transliteration error:", error);
+    }
+  }
 
   return (
-    <div className="container">
-      <div className="row justify-content-center my-5">
+    <form
+      className="container text-center"
+      onSubmit={handleSubmit}
+    >
+      <h1 className="my-5 fs-2 text-secondary">Сервис транслитерации</h1>
+      <div className="row justify-content-center gap-5">
         <div className="col-5">
-          <h2 className="text-center mb-5">Сервис транслитерации</h2>
-          <div className="form-floating mb-3">
-            <input
-              id="text"
-              type="text"
-              placeholder="Текст"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              className="form-control"
-            />
-            <label htmlFor="text" className="text-secondary">
-              Текст
-            </label>
+          <div className="d-flex justify-content-between align-items-center mb-2">
+            <span className="fs-4">Ввод</span>
+            {text && (
+              <button
+                type="button"
+                title="Очистить"
+                onClick={handleClear}
+                className="btn btn-close"
+              ></button>
+            )}
           </div>
-          <div className="form-floating">
-            <input
-              id="transliteration"
-              type="text"
-              placeholder="Транслитерация"
-              value={transliterate(text)}
-              readOnly={true}
-              className="form-control"
+          <div className="input-group">
+            <textarea
+              id="text"
+              value={text}
+              placeholder="Введите текст"
+              onChange={(e) => setText(e.target.value)}
+              style={{ minHeight: 250 }}
+              className="form-control p-3"
             />
-            <label htmlFor="transliteration" className="text-secondary">
-              Транслитерация
-            </label>
+          </div>
+        </div>
+        <div className="col-5">
+          <div className="d-flex justify-content-between align-items-center mb-2">
+            <span className="fs-4">Результат</span>
+          </div>
+          <div className="input-group">
+            <textarea
+              id="translit"
+              value={translit}
+              readOnly={true}
+              style={{ minHeight: 250 }}
+              className="form-control p-3"
+            />
           </div>
         </div>
       </div>
-    </div>
+      <button
+        type="submit"
+        disabled={!text}
+        className="btn btn-success mt-4 px-5 py-2 fs-5"
+      >
+        Выполнить
+      </button>
+    </form>
   );
 };
